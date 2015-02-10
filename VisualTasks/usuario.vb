fdf1d1067f2b9b8685dd7199e17cf4ec9075c403
Gorriti <gorriti@hotmail.com>
2015-02-10 22:55:01 +0100

    create User

---
+++ VisualTasks/usuario.vb
@@ -0,0 +1,17 @@
+﻿Public Class usuario
+
+    Private Sub UserBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles UserBindingNavigatorSaveItem.Click
+
+        'TODO: Controlar que no exista el UserName
+        Me.Validate()
+        Me.UserBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.ScrumDataSet)
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'ScrumDataSet.User' Puede moverla o quitarla según sea necesario.
+        Me.UserTableAdapter.Fill(Me.ScrumDataSet.User)
+
+    End Sub
+End Class
