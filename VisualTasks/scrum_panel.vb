be5a4d1f8b87687dd05e9741e36707f26343a403
uesteibar <uesteibar@gmail.com>
2015-02-12 00:29:52 +0100

    clear lists before loading

--- VisualTasks/scrum_panel.vb
+++ VisualTasks/scrum_panel.vb
@@ -88,6 +88,11 @@
 
     Private Sub loadTasks()
         'load tasks from story
+        lstDOING.Items.Clear()
+        lstDONE.Items.Clear()
+        lstToVerify.Items.Clear()
+        lstDONE.Items.Clear()
+
         Try
             Dim conexion As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Scrum.mdb;")
             conexion.Open()
@@ -141,6 +146,11 @@
 
     Private Sub loadAllTasks()
         'TODO - load all tasks
+        lstDOING.Items.Clear()
+        lstDONE.Items.Clear()
+        lstToVerify.Items.Clear()
+        lstDONE.Items.Clear()
+
         Try
             Dim conexion As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Scrum.mdb;")
             conexion.Open()
