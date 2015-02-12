79132950f6d908908516819e49423331db97779b
Gorriti <gorriti@hotmail.com>
2015-02-12 02:07:41 +0100

    Scrum title

--- VisualTasks/scrum_panel.vb
+++ VisualTasks/scrum_panel.vb
@@ -19,10 +19,36 @@
     End Sub
 
     Private Sub scrup_panel_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        loadTitle()
         loadStories()
         initView()
     End Sub
+    Private Sub loadTitle()
+        Try
+            Dim conexion As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Scrum.mdb;")
+            conexion.Open()
 
+            Dim cmd As New OleDb.OleDbCommand
+            cmd.Connection = conexion
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "SELECT NombreProyecto FROM Proyecto WHERE ProyectoID=" & projectID
+
+            Dim dr As OleDb.OleDbDataReader
+
+            dr = cmd.ExecuteReader
+
+            While dr.Read
+                Me.Text = dr(0)
+            End While
+
+            dr.Close()
+
+            conexion.Close()
+        Catch ex As Exception
+            MsgBox("Error de conexion:" & ex.Message)
+            Me.Close()
+        End Try
+    End Sub
     Private Sub loadStories()
         lstStories.Items.Clear()
         'load stories
